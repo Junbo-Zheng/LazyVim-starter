3913d44e073002c072cfe6a47cc4646de1a12a78
Junbo Zheng <zhengjunbo1@xiaomi.com>
2024-08-25 23:00:09 +0800

    keymaps: add keymap_set from '9' to '$'
    Signed-off-by: Junbo Zheng <zhengjunbo1@xiaomi.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+LazyVim.safe_keymap_set({ "n", "x" }, "9", "'$'", { desc = "Line", expr = true, silent = true })
