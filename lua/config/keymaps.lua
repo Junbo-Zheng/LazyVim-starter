9fa9754321a1e3880ebfe7cfeca2feaacb1920fb
Junbo Zheng <zhengjunbo1@xiaomi.com>
2024-09-09 00:08:28 +0800

    keymaps: add keymap_set from '-' to '$'
    Signed-off-by: Junbo Zheng <zhengjunbo1@xiaomi.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-LazyVim.safe_keymap_set({ "n", "x" }, "9", "'$'", { desc = "Line", expr = true, silent = true })
+LazyVim.safe_keymap_set({ "n", "x" }, "-", "'$'", { desc = "Line", expr = true, silent = true })
